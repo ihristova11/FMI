a3432a9ae1bac26e07b19371ee005bad0972df1f
Irina Hristova <hristova119@gmail.com>
2018-12-06 14:16:59 +0200

    Renamed files and added solution to task

---
+++ 06. Functions and recursion/01.sum-of-two-numbers.cpp
@@ -0,0 +1,15 @@
+//Write a function that takes two numbers as parameters and returns their sum.
+
+#include<iostream>
+
+using namespace std;
+
+int sum(int a, int b)
+{
+	return (a + b);
+}
+
+int main()
+{
+	cout << sum << endl;
+}
